d2b6f3b3a073e22992633135ce8e6d490a5f4afa
Jee hyun PARK <jeepark@paul-f3Br4s1.clusters.42paris.fr>
2025-05-03 18:08:17 +0200

    return tmdb id

--- backend/history/serializers.py
+++ backend/history/serializers.py
@@ -3,7 +3,9 @@ from .models import WatchHistory
 
 
 class WatchHistorySerializer(serializers.ModelSerializer):
+    tmdb_id = serializers.IntegerField(source='movie.tmdb_id', read_only=True)
+
     class Meta:
         model = WatchHistory
-        fields = ["id", "user", "movie", "watched_at"]
-        read_only_fields = ["id", "user", "watched_at"]
+        fields = ["id", "user", "movie", "watched_at", "tmdb_id"]
+        read_only_fields = ["id", "user", "watched_at", "tmdb_id"]
